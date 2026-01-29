606e176585ba20727d55a5c2da42a9fe6aaf51b1
unnioc <wyx179179@qq.com>
2026-01-30 03:07:12 +0800

    feat：定义全局大图标样式

--- App.vue
+++ App.vue
@@ -1,23 +1,28 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  }
+}
 </script>
 
 <style>
-	/*每个页面公共css */
-	
-	/* 图标通用大小 */
-	.icon {
-		width: 20px;
-		height: 20px;
-	}
+/*每个页面公共css */
+
+/* 图标通用大小 */
+.icon {
+  width: 40rpx;
+  height: 40rpx;
+}
+
+.icon-l {
+  width: 60rpx;
+  height: 60rpx;
+}
 </style>
